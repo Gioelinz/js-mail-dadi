9705614df771e0f3a4b7ccbae917a9cbe43ce609
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-10 16:04:27 +0100

    add mail system

--- js/mail.js
+++ js/mail.js
@@ -3,3 +3,14 @@
 // Chiedi all’utente la sua email.
 // controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
 
+const mails = ['fizzbuzz@gmail.com', 'fizz@gmail.com', 'buzz@gmail.com'];
+
+let mailAnswer = prompt('Inserisci la tua mail', 'fizz@gmail.com');
+
+for (let i = 0; i < mails.length; i++) {
+    if (mailAnswer == mails) {
+        console.log('Controllo Ok, Puoi accedere!')
+    } else {
+        console.log('La mail non è stata trovata nella lista, riprova!')
+    }
+}
