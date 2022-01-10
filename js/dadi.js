7ee48479b8a642c29b0e28bcfb4f130761995ff9
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-10 16:25:48 +0100

    add dadi game

--- js/dadi.js
+++ js/dadi.js
@@ -2,3 +2,21 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Stampare in console i tiri di dado e il risultato.
+
+let user
+let pc
+
+for (let i = 0; i < 1; i++) {
+    user = Math.floor(Math.random() * 6) + 1;
+    pc = Math.floor(Math.random() * 6) + 1;
+    console.log(`Tuo punteggio: ${user} Punteggio avversario: ${pc}`)
+
+    if (user > pc) {
+        console.log("Hai vinto!")
+    } else if (user < pc) {
+        console.log("Hai perso!")
+    } else {
+        console.log("Pareggio!")
+    }
+}
+
