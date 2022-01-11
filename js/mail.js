2ab6fb6eb4dd990e589e112888400ac7d2f6defd
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-11 17:34:16 +0100

    fix & onpage stamp

--- js/mail.js
+++ js/mail.js
@@ -3,14 +3,22 @@
 // Chiedi all’utente la sua email.
 // controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
 
+const displayElement = document.getElementById("display");
+
 const mails = ['fizzbuzz@gmail.com', 'fizz@gmail.com', 'buzz@gmail.com'];
 
 let mailAnswer = prompt('Inserisci la tua mail', 'fizz@gmail.com');
 
 for (let i = 0; i < mails.length; i++) {
-    if (mailAnswer == mails[i]) {
-        console.log('Controllo Ok, Puoi accedere!')
-    } else if (!(mailAnswer == mails[i])) {
-        console.log('La mail non è stata trovata nella lista, riprova!')
+    if (mailAnswer === mails[i]) {
+        console.log('Trovato');
+    } else {
+        console.log("Non trovato");
     }
 }
+
+const isAllowed = mails.includes(mailAnswer);
+
+const message = isAllowed === true ? `Benvenuto ${mailAnswer}` : 'Spiacente non sei autorizzato';
+
+displayElement.innerText = message;
